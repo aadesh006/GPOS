fd069f2ed63d3183cf856858ffea09d5262353d8
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2025-12-29 16:18:59 +0530

    Created fn to write to terminal

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,8 @@
 #include <stddef.h>
 
 uint16_t* video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
 uint16_t terminal_make_char(char c, char colour)
 {
@@ -14,9 +16,29 @@ void terminal_putchar( int x, int y, char c, char colour)
     video_mem[(y*VGA_WIDTH) + x] = terminal_make_char(c, colour);
 }
 
+void terminal_writechar(char c, char colour)
+{
+    if (c == '\n')
+    {
+        terminal_row += 1;
+        terminal_col = 0;
+        return;
+    }
+    
+    terminal_putchar(terminal_col, terminal_row, c, colour);
+    terminal_col += 1;
+    if(terminal_col >= VGA_WIDTH)
+    {
+        terminal_col = 0;
+        terminal_row += 1;
+    }
+}
+
 void terminal_intialize()
 {
     video_mem = (uint16_t*)(0xB8000);
+    terminal_col =0;
+    terminal_row = 0;
     for (int y =0; y < VGA_HEIGHT; y++)
     {
         for(int x = 0; x<VGA_WIDTH; x++)
@@ -37,11 +59,19 @@ size_t strlen(const char* str)
     return len;
 }
 
+void print(const char* str)
+{
+    size_t len = strlen(str);
+    for(int i =0; i<len; i++)
+    {
+        terminal_writechar(str[i], 5);
+    }
+}
 
 
 void kernel_main()
 {
     terminal_intialize();
-    video_mem[0] = terminal_make_char('B',3);
+    print("Hello World!");
 
 }
