9b68ebb57f7743986e8b4d6d0fccc385147afc17
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2025-12-29 15:41:42 +0530

    String Count fn in kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,6 @@
 #include "kernel.h"
 #include <stdint.h>
+#include <stddef.h>
 
 uint16_t* video_mem = 0;
 
@@ -8,8 +9,14 @@ uint16_t terminal_make_char(char c, char colour)
     return (colour << 8) | c;
 }
 
-void terminal_intialize(){
-    video_mem = (int16_t*)(0xB8000);
+void terminal_putchar( int x, int y, char c, char colour)
+{
+    video_mem[(y*VGA_WIDTH) + x] = terminal_make_char(c, colour);
+}
+
+void terminal_intialize()
+{
+    video_mem = (uint16_t*)(0xB8000);
     for (int y =0; y < VGA_HEIGHT; y++)
     {
         for(int x = 0; x<VGA_WIDTH; x++)
@@ -20,9 +27,21 @@ void terminal_intialize(){
     }
 }
 
+size_t strlen(const char* str)
+{
+    size_t len = 0;
+    while(str[len])
+    {
+        len ++;
+    }
+    return len;
+}
+
+
 
 void kernel_main()
 {
+    terminal_intialize();
     video_mem[0] = terminal_make_char('B',3);
 
 }
